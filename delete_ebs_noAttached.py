dad85fb456619cda123425fbb299872ded5fa98c
Vitor Esteves <vesteves33@gmail.com>
2025-12-16 17:30:00 -0300

    fix: Ajustes nos arquivos e nas etapas da pipeline para rodar conforme o esperado e passar nos testes

--- delete_ebs_noAttached.py
+++ delete_ebs_noAttached.py
@@ -1,50 +1,52 @@
-import os
 import csv
 import json
-import boto3
-from io import StringIO
+import os
 from datetime import datetime
-from logging import basicConfig, info, error, INFO
+from io import StringIO
+from logging import INFO, basicConfig, error, info
 
-#Variaveis de ambiente passadas através do AWS Lambda
+import boto3
+
+# Variaveis de ambiente passadas através do AWS Lambda
 BUCKET_NAME = os.environ.get("TARGET_BUCKET_S3")
 SNS_TOPIC_ARN = os.environ.get("SNS_TOPIC_ARN")
 REGIONS = os.environ.get("TARGET_REGIONS", "[]")
 
-#Criando um logging para o código
+# Criando um logging para o código
 basicConfig(level=INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
+
 def get_aws_account_name_and_id() -> tuple:
-    '''
+    """
     Obtém o nome e ID da conta AWS.
 
-    Returns: 
+    Returns:
         Retorna uma tupla contendo o nome da conta e o ID da conta.
-    '''
-    #Obtem id da conta AWS
+    """
+    # Obtem id da conta AWS
     sts_client = boto3.client("sts")
     account_id = sts_client.get_caller_identity().get("Account")
-    
+
     # Obtem o nome da conta
     iam_client = boto3.client("iam")
     account_name = ""
     try:
         account_aliases = iam_client.list_account_aliases().get("AccountAliases", [])
-        
+
         if account_aliases:
             info("Obtendo o primeiro alias da conta AWS")
-            account_name = account_aliases[0]  
+            account_name = account_aliases[0]
         else:
             info("Nenhum alias encontrado para a conta AWS, usando '-' como nome da conta")
             account_name = "-"
     except Exception as e:
         error(f"Erro ao obter o nome da conta AWS: {e}")
-    
+
     return account_name, account_id
 
 
 def upload_file_to_s3(file_path: str, bucket_name: str, object_name: str) -> bool:
-    '''
+    """
     Realiza o upload de um arquivo para um Bucket S3.
 
     Args:
@@ -52,12 +54,12 @@ def upload_file_to_s3(file_path: str, bucket_name: str, object_name: str) -> boo
      bucket_name (str): Nome do bucket S3 de destino.
      object_name (str): Nome do objeto S3 (caminho completo no bucket).
                         Se não for fornecido, o nome base do file_path será usado.
-    Return: 
+    Return:
         Retorna verdadeiro para o upload bem sucedido.
-    '''
+    """
 
     client = boto3.client("s3")
-    
+
     if not bucket_name:
         error("O nome do bucket S3 não foi fornecido verificar as variaveis de ambiente.")
         return False
@@ -67,42 +69,38 @@ def upload_file_to_s3(file_path: str, bucket_name: str, object_name: str) -> boo
         client.put_object(Bucket=bucket_name, Key=object_name, Body=file_path)
         info(f"Upload do arquivo {file_path} concluído com sucesso.")
         return True
-    
+
     except Exception as e:
         error(f"Ocorreu um erro inesperado durante o upload: {e}")
         return False
-    
-def send_email(count_ebs_excluidos_por_regiao: int,count_ebs_nao_excluidos_por_regiao: int) -> None:
+
+
+def send_email(count_ebs_excluidos_por_regiao: int, count_ebs_nao_excluidos_por_regiao: int) -> None:
     account_name, account_id = get_aws_account_name_and_id()
 
     # Publicar mensagem no tópico SNS
     sns_topic_arn = SNS_TOPIC_ARN
     if not sns_topic_arn:
         error("O ARN do tópico SNS não foi fornecido verificar as variaveis de ambiente.")
-        return {'message':'O ARN do tópico SNS não foi fornecido verificar as variaveis de ambiente.'}
-    
+        return {"message": "O ARN do tópico SNS não foi fornecido verificar as variaveis de ambiente."}
+
     sns_client = boto3.client("sns")
 
     # Formatando a mensagem para incluir o número de EBS excluídos e não excluídos em cada região
     message = "Olá,\n\n"
-    message += (
-        "A função Lambda para exclusão de EBS unattached foi executada com sucesso!\n\n"
-    )
+    message += "A função Lambda para exclusão de EBS unattached foi executada com sucesso!\n\n"
     message += "Aqui está um resumo dos resultados:\n\n"
     message += "Região\t\tExcluídos\tNão Excluídos\n"
     message += "--------------------------------------------------\n"
     for regiao, count_excluidos in count_ebs_excluidos_por_regiao:
         count_nao_excluidos = next(
-            (
-                count
-                for reg, count in count_ebs_nao_excluidos_por_regiao
-                if reg == regiao
-            ),
+            (count for reg, count in count_ebs_nao_excluidos_por_regiao if reg == regiao),
             0,
         )
         message += f"{regiao}\t\t{count_excluidos}\t\t{count_nao_excluidos}\n"
     message += "--------------------------------------------------\n"
-    message += f"Total de EBS verificados: {sum([count for _, count in count_ebs_excluidos_por_regiao]) + sum([count for _, count in count_ebs_nao_excluidos_por_regiao])}.\n"
+    message += f"""Total de EBS verificados: {sum([count for _, count in count_ebs_excluidos_por_regiao]) +
+                                              sum([count for _, count in count_ebs_nao_excluidos_por_regiao])}.\n"""
     message += f"Total de EBS excluídos: {sum([count for _, count in count_ebs_excluidos_por_regiao])}.\n"
     message += f"Total de EBS não excluídos: {sum([count for _, count in count_ebs_nao_excluidos_por_regiao])}.\n"
 
@@ -112,36 +110,37 @@ def send_email(count_ebs_excluidos_por_regiao: int,count_ebs_nao_excluidos_por_r
 
 
 def create_csv_file(dict_data: dict):
-    '''
+    """
     Cria um arquivo CSV a partir de um dicionário de dados.
 
-    Args: 
+    Args:
         dict_data (dict): Dicionário contendo os dados a serem escritos no arquivo CSV.
     Return:
         Retorna o arquivo CSV criado com os dados salvos.
-    '''
+    """
 
     output_file = StringIO()
 
     header_list = list(dict_data[0].keys())
 
     csv_writer = csv.DictWriter(output_file, fieldnames=header_list)
-    csv_writer.writeheader() 
-    csv_writer.writerows(dict_data) 
+    csv_writer.writeheader()
+    csv_writer.writerows(dict_data)
 
     csv_file = output_file.getvalue()
     info(f"CSV criado com {len(dict_data)} registros")
-    output_file.close() 
+    output_file.close()
 
     return csv_file
 
+
 # funcao lambda principal
 def handler(event, context):
-    count_ebs_excluidos_por_regiao = ( [] )  
-    count_ebs_nao_excluidos_por_regiao = ( [] )
+    count_ebs_excluidos_por_regiao = []
+    count_ebs_nao_excluidos_por_regiao = []
 
     volumes_list = []
-    
+
     regions = json.loads(REGIONS)
     for region in regions:
         print(f"#### {region} ####")
@@ -168,32 +167,32 @@ def handler(event, context):
 
                     # Verificar se o volume possui a tag 'inUse' com valor True
                     if "VolumeState" in tags and tags["VolumeState"].lower() == "inuse":
-                        print(
-                            f'Volume {volume["VolumeId"]} não pode ser excluído pois está marcado como em uso'
-                        )
+                        print(f"Volume {volume['VolumeId']} não pode ser excluído pois está marcado como em uso")
                         ebs_nao_excluidos.append(volume["VolumeId"])
                         count_ebs_nao_excluidos += 1
                     else:
-                    #Inclusao dos ebs que vao ser excluidos em uma lista para posterior upload em bucket s3    
-                        volumes_list.append({
-                            "ListedDate": datetime.now().strftime("%Y-%m-%d"),
-                            "VolumeId":volume["VolumeId"],
-                            "VolumeType":volume["VolumeType"],
-                            "Region":volume["AvailabilityZone"],
-                            "SizeGb":volume["Size"],
-                            "State":volume["State"],
-                            "Iops":volume.get("Iops",'N/A'),
-                            "Tags":tags,
-                            })
-                        info(f'Volume {volume["VolumeId"]} incluso na listagem para exclusão')
-                        
+                        # Inclusao dos ebs que vao ser excluidos em uma lista para posterior upload em bucket s3
+                        volumes_list.append(
+                            {
+                                "ListedDate": datetime.now().strftime("%Y-%m-%d"),
+                                "VolumeId": volume["VolumeId"],
+                                "VolumeType": volume["VolumeType"],
+                                "Region": volume["AvailabilityZone"],
+                                "SizeGb": volume["Size"],
+                                "State": volume["State"],
+                                "Iops": volume.get("Iops", "N/A"),
+                                "Tags": tags,
+                            }
+                        )
+                        info(f"Volume {volume['VolumeId']} incluso na listagem para exclusão")
+
                         # Se o volume não possuiu a tag 'inUse', pode ser excluído
                         ec2.delete_volume(VolumeId=volume["VolumeId"])
-                        print(f'Volume {volume["VolumeId"]} excluído com sucesso')
+                        print(f"Volume {volume['VolumeId']} excluído com sucesso")
                         ebs_excluidos.append(volume["VolumeId"])
                         count_ebs_excluidos += 1
                 else:
-                    print(f'O volume {volume["VolumeId"]} está anexado a uma instância')
+                    print(f"O volume {volume['VolumeId']} está anexado a uma instância")
                     ebs_nao_excluidos.append(volume["VolumeId"])
                     count_ebs_nao_excluidos += 1
 
@@ -212,37 +211,34 @@ def handler(event, context):
         count_ebs_nao_excluidos_por_regiao.append((region, count_ebs_nao_excluidos))
 
     # enviar um email com o número de EBS excluídos e não excluídos em cada região
-    send_email(count_ebs_excluidos_por_regiao,count_ebs_nao_excluidos_por_regiao)
+    send_email(count_ebs_excluidos_por_regiao, count_ebs_nao_excluidos_por_regiao)
 
     # Criando arquivo CSV com os volumes que foram excluídos
     csv_file = create_csv_file(volumes_list)
-    if not csv_file:    
+    if not csv_file:
         error("Erro ao criar o arquivo CSV")
-        return {
-            'statusCode': 500,
-            'body': 'Erro ao criar o arquivo CSV'
-        }
-    
+        return {"statusCode": 500, "body": "Erro ao criar o arquivo CSV"}
+
     # Definindo o nome do arquivo CSV
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     csv_object_key = f"report/unattached_ebs_{timestamp}.csv"
 
     if not BUCKET_NAME:
-            error("O nome do bucket S3 não foi fornecido verificar as variaveis de ambiente.")
-            return {
-                'statusCode': 500,
-                'body': 'O nome do bucket S3 não foi fornecido verificar as variaveis de ambiente.'
-            }
-        
+        error("O nome do bucket S3 não foi fornecido verificar as variaveis de ambiente.")
+        return {
+            "statusCode": 500,
+            "body": "O nome do bucket S3 não foi fornecido verificar as variaveis de ambiente.",
+        }
+
     if upload_file_to_s3(csv_file, BUCKET_NAME, csv_object_key):
         info(f"Arquivo CSV {csv_object_key} enviado para o bucket S3 {BUCKET_NAME} com sucesso.")
         return {
-            'statusCode': 200,
-            'body': f'Arquivo CSV {csv_object_key} enviado para o bucket S3 {BUCKET_NAME}.'
+            "statusCode": 200,
+            "body": f"Arquivo CSV {csv_object_key} enviado para o bucket S3 {BUCKET_NAME}.",
         }
     else:
         error(f"Falha ao enviar o arquivo CSV {csv_object_key} para o bucket S3 {BUCKET_NAME}.")
         return {
-            'statusCode': 500,
-            'body': 'Falha ao enviar o arquivo CSV para o bucket S3.'
+            "statusCode": 500,
+            "body": "Falha ao enviar o arquivo CSV para o bucket S3.",
         }
--- estimate_ebs_noAttached.py
+++ estimate_ebs_noAttached.py
@@ -1,108 +1,105 @@
-import os
 import csv
-from io import StringIO
-from boto3 import client
+import os
 from datetime import datetime, timedelta
-from logging import basicConfig, info, error, INFO
+from io import StringIO
+from logging import INFO, basicConfig, error, info
 
-#Variaveis de ambiente passadas através do AWS Lambda
+from boto3 import client
+
+# Variaveis de ambiente passadas através do AWS Lambda
 BUCKET_NAME = os.environ.get("TARGET_BUCKET_S3")
 FOLDER_PREFIX = os.environ.get("TARGET_BUCKET_S3_FOLDER")
 
-#Criando um logging para o código
+# Criando um logging para o código
 basicConfig(level=INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
+
 def get_data_csv_report_file():
-  '''
+    """
     Obtém o arquivo CSV mais recente do bucket S3 especificado e extrai os IDs de recursos (VolumeId) do EBS.
-  '''
-  s3 = client('s3')
+    """
+    s3 = client("s3")
 
-  latest_file = None
-  latest_modified_time = datetime.min
-  list_id_resources = []  
+    latest_file = None
+    latest_modified_time = datetime.min
+    list_id_resources = []
 
-  try:
-    response = s3.list_objects_v2(Bucket=BUCKET_NAME, Prefix=FOLDER_PREFIX)
-
-    if 'Contents' in response:
-        for obj in response['Contents']:
-            # Verifica se é um arquivo CSV e não é a própria pasta (que geralmente termina com /)
-            if obj['Key'].endswith('.csv') and not obj['Key'].endswith('/'):
-                if obj['LastModified'].replace(tzinfo=None) > latest_modified_time:
-                    latest_modified_time = obj['LastModified'].replace(tzinfo=None)
-                    latest_file = obj['Key']
-
-    if not latest_file:
-        error(f"Nenhum arquivo CSV encontrado na pasta {FOLDER_PREFIX} do bucket {BUCKET_NAME}.")
-        return None
-    
-    # Obtem o conteúdo do arquivo
-    obj_content = s3.get_object(Bucket=BUCKET_NAME, Key=latest_file)
-    file_body = obj_content['Body'].read().decode('utf-8')
-    
-    info("Transformando o objeto string em csv")
-    csv_file = StringIO(file_body)
-    reader = csv.reader(csv_file)
-
-    header = next(reader, None)
-    if header is None or 'VolumeId' not in header:
-        error("O CSV está vazio ou não possui cabeçalho.")
-        return None
-    
     try:
-        volume_id_index = header.index('VolumeId')
-    except ValueError:
-        error("A coluna 'VolumeId' não foi encontrada no cabeçalho do CSV.")
+        response = s3.list_objects_v2(Bucket=BUCKET_NAME, Prefix=FOLDER_PREFIX)
+
+        if "Contents" in response:
+            for obj in response["Contents"]:
+                # Verifica se é um arquivo CSV e não é a própria pasta (que geralmente termina com /)
+                if obj["Key"].endswith(".csv") and not obj["Key"].endswith("/"):
+                    if obj["LastModified"].replace(tzinfo=None) > latest_modified_time:
+                        latest_modified_time = obj["LastModified"].replace(tzinfo=None)
+                        latest_file = obj["Key"]
+
+        if not latest_file:
+            error(f"Nenhum arquivo CSV encontrado na pasta {FOLDER_PREFIX} do bucket {BUCKET_NAME}.")
+            return None
+
+        # Obtem o conteúdo do arquivo
+        obj_content = s3.get_object(Bucket=BUCKET_NAME, Key=latest_file)
+        file_body = obj_content["Body"].read().decode("utf-8")
+
+        info("Transformando o objeto string em csv")
+        csv_file = StringIO(file_body)
+        reader = csv.reader(csv_file)
+
+        header = next(reader, None)
+        if header is None or "VolumeId" not in header:
+            error("O CSV está vazio ou não possui cabeçalho.")
+            return None
+
+        try:
+            volume_id_index = header.index("VolumeId")
+        except ValueError:
+            error("A coluna 'VolumeId' não foi encontrada no cabeçalho do CSV.")
+            return None
+
+        for row in reader:
+            if row:
+                list_id_resources.append(row[volume_id_index])
+
+        info("Retornando a lista de IDs de recursos")
+        return list_id_resources
+
+    except Exception as e:
+        error(f"Ocorreu um erro ao acessar o S3: {e}")
         return None
-    
-    for row in reader:
-        if row: 
-            list_id_resources.append(row[volume_id_index])
-    
-    info("Retornando a lista de IDs de recursos")
-    return list_id_resources
-    
-  except Exception as e:
-    error(f"Ocorreu um erro ao acessar o S3: {e}")
-    return None
 
 
 def get_daily_cost_for_resource(resource_id, start_date, end_date):
-    '''
-      Consulta o AWS Cost Explorer para obter o custo diário de um recurso específico.
-    '''
-    ce = client('ce')
+    """
+    Consulta o AWS Cost Explorer para obter o custo diário de um recurso específico.
+    """
+    ce = client("ce")
 
     try:
         response = ce.get_cost_and_usage_with_resources(
-            TimePeriod={
-                'Start': start_date,
-                'End': end_date
-            },
-            Granularity= 'DAILY',
+            TimePeriod={"Start": start_date, "End": end_date},
+            Granularity="DAILY",
             Metrics=[
-                'UnblendedCost',
+                "UnblendedCost",
             ],
             GroupBy=[
-                {'Type': 'DIMENSION', 'Key': 'RESOURCE_ID'},
-                {'Type': 'DIMENSION', 'Key': 'RECORD_TYPE'},
+                {"Type": "DIMENSION", "Key": "RESOURCE_ID"},
+                {"Type": "DIMENSION", "Key": "RECORD_TYPE"},
             ],
-            Filter={
-                'Dimensions': {'Key': 'RECORD_TYPE', 'Values': ['Usage']}
-            }                
+            Filter={"Dimensions": {"Key": "RECORD_TYPE", "Values": ["Usage"]}},
         )
 
         costs = {}
-        for result_by_time in response['ResultsByTime']:
-            date = result_by_time['TimePeriod']['Start']
+        for result_by_time in response["ResultsByTime"]:
+            date = result_by_time["TimePeriod"]["Start"]
             # O custo vem como string, converte para float
-            for group in result_by_time['Groups']:
-                actual_resource_id = group['Keys'][0]
-                actual_record_type = group['Keys'][1]
+            for group in result_by_time["Groups"]:
+                actual_resource_id = group["Keys"][0]
+                actual_record_type = group["Keys"][1]
 
-                if actual_resource_id == resource_id and actual_record_type == 'Usage':
-                    amount = float(group['Metrics']['UnblendedCost']['Amount'])
+                if actual_resource_id == resource_id and actual_record_type == "Usage":
+                    amount = float(group["Metrics"]["UnblendedCost"]["Amount"])
                     costs[date] = amount
                     break
         return costs
@@ -113,39 +110,33 @@ def get_daily_cost_for_resource(resource_id, start_date, end_date):
 
 
 def handler(event, context):
-  '''
+    """
     Função Lambda que inicia o processo de estimativa de EBS sem volumes anexados.
-  '''
-  
-  try:
-    info("Iniciando o processo de estimativa de EBS sem volumes anexados.")
-    
-    resources = get_data_csv_report_file()
-    
-    for resource in resources:
-      info(f"Consultando custo diário para o recurso {resource}")
-      start_date = (datetime.now() - timedelta(days=14)).strftime('%Y-%m-%d')
-      end_date = datetime.now().strftime('%Y-%m-%d')
-      
-      daily_costs = get_daily_cost_for_resource(str(resource), start_date, end_date)
-      
-      if daily_costs:
-        info(f"Custo diário para o recurso {resource}: {daily_costs}")
-        print(f"Custo diário para o recurso {resource}: {daily_costs}")
-      else:
-        error(f"Não foi possível obter o custo para o recurso {resource}")
+    """
 
-    info("Concluido com sucesso!")
-    
-    return {
-            'statusCode': 200,
-            'body': "Sucesso"
-        }
+    try:
+        info("Iniciando o processo de estimativa de EBS sem volumes anexados.")
 
-  except Exception as e:
-    error(f"Erro capturado {e}")
-    
-    return {
-            'statusCode': 500,
-            'body': f'Erro capturado: {e}'
-        }
+        resources = get_data_csv_report_file()
+
+        for resource in resources:
+            info(f"Consultando custo diário para o recurso {resource}")
+            start_date = (datetime.now() - timedelta(days=14)).strftime("%Y-%m-%d")
+            end_date = datetime.now().strftime("%Y-%m-%d")
+
+            daily_costs = get_daily_cost_for_resource(str(resource), start_date, end_date)
+
+            if daily_costs:
+                info(f"Custo diário para o recurso {resource}: {daily_costs}")
+                print(f"Custo diário para o recurso {resource}: {daily_costs}")
+            else:
+                error(f"Não foi possível obter o custo para o recurso {resource}")
+
+        info("Concluido com sucesso!")
+
+        return {"statusCode": 200, "body": "Sucesso"}
+
+    except Exception as e:
+        error(f"Erro capturado {e}")
+
+        return {"statusCode": 500, "body": f"Erro capturado: {e}"}
